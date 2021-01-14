184935db367f657799c241c4359452ccaf1711c3
architolk <msj@xs4all.nl>
2021-01-14 22:40:50 +0100

    Test vereenvoudigd

--- src/MAX7219CNG-test/MAX7219CNG-test.ino
+++ src/MAX7219CNG-test/MAX7219CNG-test.ino
@@ -3,10 +3,9 @@
 
 /*
  Standard Arduino SPI pins:
- - pin 11 is connected to the DataIn 
- - pin 13 is connected to the CLK 
- - pin 10 is connected to LOAD 
- We have only a single MAX72XX.
+ - pin 11 is connected to the DataIn
+ - pin 13 is connected to the CLK
+ - pin 10 is connected to LOAD
  */
 
 const uint8_t MAXPIN_LOAD = 10;
@@ -19,13 +18,6 @@ const uint8_t MAXVAL_OPERATIONAL = 1;
 const uint8_t MAXVAL_NONE = 0;
 
 uint8_t rows[8] = {0,0,0,0,0,0,0,0};
-
-int button = 0;
-uint16_t aread = 0;
-
-bool buttons[9] = {false, false, false, false, false, false, false, false, false};
-
-bool startup = true;
 
 void setup() {
   pinMode(MAXPIN_LOAD, OUTPUT);
@@ -43,103 +35,18 @@ void setup() {
   setLed(0,2,true);
   setLed(0,1,true);
   setLed(0,0,true);
-  /*
-  setLed(1,6,true);
-  setLed(1,5,true);
-  setLed(1,4,true);
-  setLed(1,3,true);
-  */
   delay(3000);
 
   clearDisplay();
 
-  //Serial.begin(9600);
 }
 
 void loop() {
   for (uint8_t i=4; i<=6; i++) {
-    if (startup) {
-      setLed(0,i,true);
-      //setLed(1,i,true);
-    }
-    checkAndDelay();
-    //delay(500);
-    if (startup) {
-      setLed(0,i,false);
-      //setLed(1,i,false);
-    }
-    //Serial.println(analogRead(A0));
-    //Serial.println(buttonPressed());
+    setLed(0,i,true);
+    delay(500);
+    setLed(0,i,false);
   }
-}
-
-void checkAndDelay() {
-  for (int i = 0; i<50; i++) {
-    delay(10);
-    int b = buttonPressed();
-    if (b!=button) {
-      button=b;
-      if (button!=0 && button!=5) {
-        //Serial.print(aread);
-        //Serial.print(" >> ");
-        //Serial.println(button);
-        if (startup) {
-          startup = false;
-          clearDisplay();
-        }
-      }
-      if (button==7) {
-        startup = true;
-      }
-      if (button==1) {
-        buttons[0] = not(buttons[0]);
-        setLed(0,2,buttons[0]);
-      }
-      if (button==2) {
-        buttons[1] = not(buttons[1]);
-        setLed(0,0,buttons[1]);
-      }
-      if (button==3) {
-        buttons[2] = not(buttons[2]);
-        setLed(0,4,buttons[2]);
-      }
-      if (button==4) {
-        buttons[3] = not(buttons[3]);
-        setLed(0,5,buttons[3]);
-      }
-      if (button==5) {
-        buttons[4] = not(buttons[4]);
-        setLed(0,6,buttons[4]);
-      }
-      if (button==6) {
-        buttons[5] = not(buttons[5]);
-        setLed(0,1,buttons[5]);
-      }
-    }
-  }
-}
-
-int buttonPressed() {
-  uint16_t b = analogRead(A0);
-  aread = b;
-  /*
-  if (b>90 && b<150) return 1;
-  if (b>200 && b<250) return 2;
-  if (b>280 && b<380) return 3;
-  if (b>400 && b<500) return 4;
-  if (b>510 && b<620) return 5;
-  if (b>630 && b<710) return 6;
-  if (b>750 && b<850) return 7;
-  if (b>870 && b<960) return 8;
-  if (b>980) return 9;
-  */
-  if (b>90 && b<250) return 1;
-  if (b>300 && b<400) return 2;
-  if (b>450 && b<600) return 3;
-  if (b>650 && b<750) return 4;
-  if (b>800 && b<900) return 5;
-  if (b>950) return 6;
-  return 0;
 }
 
 void setLed(uint8_t row, uint8_t col, bool onoff) {
