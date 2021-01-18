2a15ea5e6eec7b518c3c0fda7d1c682b72abb857
architolk <msj@xs4all.nl>
2021-01-18 21:49:01 +0100

    Added remaining panels

--- src/MAX7219CNG-test/MAX7219CNG-test.ino
+++ src/MAX7219CNG-test/MAX7219CNG-test.ino
@@ -17,7 +17,7 @@ const uint16_t MAXOP_SHUTDOWN = 12;
 const uint8_t MAXVAL_OPERATIONAL = 1;
 const uint8_t MAXVAL_NONE = 0;
 
-const uint8_t BTNMAP[8][6] = {{2,0,4,5,6,1},{0,0,0,0,0,0},{0,2,4,1,6,5},{6,1,4,2,0,5},{0,0,0,0,0,0},{2,0,4,6,1,5},{6,1,4,2,0,5},{2,0,4,6,1,5}};
+const uint8_t BTNMAP[8][6] = {{2,0,4,5,6,1},{2,0,4,5,6,1},{0,2,4,1,6,5},{6,1,4,2,0,5},{2,0,4,5,6,1},{2,0,4,6,1,5},{6,1,4,2,0,5},{2,0,4,6,1,5}};
 
 uint8_t rows[8] = {0,0,0,0,0,0,0,0};
 
@@ -31,22 +31,18 @@ void setup() {
 
   clearDisplay();
 
-  setLed(0,6,true);
-  setLed(0,5,true);
-  setLed(0,4,true);
   setLed(0,2,true);
-  setLed(0,1,true);
-  setLed(0,0,true);
 
-  setLed(5,0,true);
-  setLed(5,1,true);
   setLed(5,2,true);
 
   setLed(3,2,true);
   setLed(6,2,true);
 
   setLed(2,2,true);
-  
+
+  setLed(1,2,true);
+  setLed(4,2,true);
+
   delay(2000);
 
   //clearDisplay();
@@ -55,16 +51,20 @@ void setup() {
 
 void loop() {
   for (uint8_t i=0; i<=5; i++) {
-    setLed(0,BTNMAP[0][i],true);
-    setLed(2,BTNMAP[2][i],true);
-    setLed(3,BTNMAP[3][i],true);
-    setLed(5,BTNMAP[5][i],true);
-    setLed(6,BTNMAP[6][i],true);
-    setLed(7,BTNMAP[7][i],true);
+    setLed(0,BTNMAP[0][i],true); //Panel 1 (blue leds)
+    setLed(1,BTNMAP[1][i],true); //Panel 2 (red leds)
+    setLed(2,BTNMAP[2][i],true); //Panel 3 (green leds)
+    setLed(3,BTNMAP[3][i],true); //Panel 4a (orange leds bottom)
+    setLed(4,BTNMAP[4][i],true); //Panel 4b (orange leds middle)
+    setLed(5,BTNMAP[5][i],true); //Panel 5 (orange leds top)
+    setLed(6,BTNMAP[6][i],true); //Panel 6a (green leds left)
+    setLed(7,BTNMAP[7][i],true); //Panel 6b (green leds right)
     delay(500);
     setLed(0,BTNMAP[0][i],false);
+    setLed(1,BTNMAP[1][i],false);
     setLed(2,BTNMAP[2][i],false);
     setLed(3,BTNMAP[3][i],false);
+    setLed(4,BTNMAP[4][i],false);
     setLed(5,BTNMAP[5][i],false);
     setLed(6,BTNMAP[6][i],false);
     setLed(7,BTNMAP[7][i],false);
