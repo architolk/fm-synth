ea781926122ac2f97836cea6892263c480324268
architolk <msj@xs4all.nl>
2021-02-07 16:03:02 +0100

    Correct mapping

--- src/MAX7219CNG-Button--test/MAX7219CNG-Button--test.ino
+++ src/MAX7219CNG-Button--test/MAX7219CNG-Button--test.ino
@@ -27,8 +27,24 @@ const uint8_t MAXVAL_NONE = 0;
 //Mapping van LED-buttons
 const uint8_t BTNMAP[8][6] = {{2,0,4,5,6,1},{2,0,1,6,5,4},{0,2,4,1,6,5},{6,1,4,2,0,5},{4,0,5,2,1,6},{2,0,4,6,1,5},{6,1,4,2,0,5},{2,0,4,6,1,5}};
 
+typedef struct {
+  uint8_t segment;
+  uint8_t digit;
+  uint8_t menu;
+  uint8_t btn;
+} map_type;
+const map_type BTNMAP2[6][9] = {{{0,2,7,5},{0,0,7,4},{0,4,7,3},{0,5,7,2},{0,6,7,1},{0,1,7,0},{7,5,9,5},{7,5,9,5},{7,5,9,5}} // {7,5,9,5} means: no led
+                               ,{{1,2,5,0},{1,0,5,1},{1,1,5,2},{1,6,5,3},{1,5,5,4},{7,5,9,5},{7,5,9,5},{7,5,9,5},{7,5,9,5}}
+                               ,{{2,0,6,5},{2,2,6,4},{2,4,6,3},{2,1,6,2},{2,6,6,1},{2,5,6,0},{7,5,9,5},{7,5,9,5},{7,5,9,5}}
+                               ,{{6,0,0,1},{6,2,0,2},{6,4,0,3},{6,1,0,4},{6,6,0,5},{3,6,1,5},{3,1,1,4},{3,4,1,3},{3,2,1,2}}
+                               ,{{4,6,4,5},{4,2,3,4},{4,1,3,5},{4,5,2,5},{4,0,2,4},{4,4,2,3},{7,5,9,5},{7,5,9,5},{7,5,9,5}}
+                               ,{{5,2,8,0},{5,0,8,1},{5,4,8,2},{5,6,8,3},{5,1,8,4},{7,6,9,3},{7,4,9,2},{7,0,9,1},{7,2,9,0}}
+                               };
+
 uint8_t rows[8] = {0,0,0,0,0,0,0,0};
 uint8_t panelvalue[6] = {0,0,0,0,0,0};
+
+bool buttons[10][6]; //Current values of the buttons
 
 void setup() {
   pinMode(MAXPIN_LOAD, OUTPUT);
@@ -37,6 +53,8 @@ void setup() {
   sendData(MAXOP_DECODEMODE, MAXVAL_NONE);
   sendData(MAXOP_SCANLIMIT, 7); // 1 = Displays 0 and 1 are active
   sendData(MAXOP_INTENSITY, 10);
+
+  setupButtons();
 
   clearDisplay();
 
@@ -54,7 +72,7 @@ void setup() {
 
   delay(2000);
 
-  //clearDisplay();
+  clearDisplay();
 
   Serial.begin(9600);
 
@@ -62,8 +80,17 @@ void setup() {
 
 }
 
+void setupButtons() {
+  for (uint8_t menu=0; menu<10; menu++) {
+    for (uint8_t btn=0; btn<6; btn++) {
+      buttons[menu][btn] = false;
+    }
+  }
+}
+
 void loop() {
   for (uint8_t i=0; i<=5; i++) {
+    /*
     setLed(0,BTNMAP[0][i],true); //Panel 1 (blue leds)
     setLed(1,BTNMAP[1][i],true); //Panel 2 (red leds)
     setLed(2,BTNMAP[2][i],true); //Panel 3 (green leds)
@@ -72,6 +99,7 @@ void loop() {
     setLed(5,BTNMAP[5][i],true); //Panel 5 (orange leds top)
     setLed(6,BTNMAP[6][i],true); //Panel 6a (green leds left)
     setLed(7,BTNMAP[7][i],true); //Panel 6b (green leds right)
+    */
 
     /*
     delay(500);
@@ -79,6 +107,7 @@ void loop() {
     */
     delayAndScan();
 
+    /*
     setLed(0,BTNMAP[0][i],false);
     setLed(1,BTNMAP[1][i],false);
     setLed(2,BTNMAP[2][i],false);
@@ -87,6 +116,7 @@ void loop() {
     setLed(5,BTNMAP[5][i],false);
     setLed(6,BTNMAP[6][i],false);
     setLed(7,BTNMAP[7][i],false);
+    */
   }
 }
 
@@ -99,16 +129,16 @@ void delayAndScan() {
 
 void readAndDisplay() {
   uint8_t p[6] = {0,0,0,0,0,0};
-  p[0] = analogRead(A9)/171; //Range between 0 and 6 (six buttons on pannel 1, 0 means no button pressed)
-  p[1] = analogRead(A8)/205; //Range between 0 and 5 (five buttons on panel 2)
-  p[2] = analogRead(A3)/171; //Range between 0 and 6 (six buttons on panel 3)
-  p[3] = analogRead(A6)/114; //Range between 0 and 9 (nine buttons on panel 4)
-  p[4] = analogRead(A7)/171; //Range between 0 and 6 (six buttons on panel 5)
-  p[5] = analogRead(A2)/114; //Range between 0 and 9 (nine buttons on panel 6)
-  for (int i=0; i<6 i++) {
+  p[0] = (analogRead(A9)+85)/171; //Range between 0 and 6 (six buttons on pannel 1, 0 means no button pressed)
+  p[1] = (analogRead(A8)+102)/205; //Range between 0 and 5 (five buttons on panel 2)
+  p[2] = (analogRead(A3)+85)/171; //Range between 0 and 6 (six buttons on panel 3)
+  p[3] = (analogRead(A6)+57)/114; //Range between 0 and 9 (nine buttons on panel 4)
+  p[4] = (analogRead(A7)+85)/171; //Range between 0 and 6 (six buttons on panel 5)
+  p[5] = (analogRead(A2)+57)/114; //Range between 0 and 9 (nine buttons on panel 6)
+  for (int i=0; i<6; i++) {
     if (panelvalue[i]!=p[i]) {
       if (panelvalue[i]==0) {
-        buttonPressed(0,p[i]) //Button down
+        buttonPressed(i,p[i]); //Button down
       }
       panelvalue[i]=p[i];
     }
@@ -125,10 +155,21 @@ void readAndDisplay() {
 }
 
 void buttonPressed(uint8_t panel, uint8_t button) {
+  uint8_t segment = BTNMAP2[panel][button-1].segment;
+  uint8_t digit = BTNMAP2[panel][button-1].digit;
+  uint8_t menu = BTNMAP2[panel][button-1].menu;
+  uint8_t btn = BTNMAP2[panel][button-1].btn;
   Serial.print("Panel: ");
   Serial.print(panel);
   Serial.print(" Button: ");
-  Serial.println(button);
+  Serial.print(button);
+  Serial.print(" Menu: ");
+  Serial.print(menu);
+  Serial.print(" Btn: ");
+  Serial.println(btn);
+
+  buttons[menu][btn] = !buttons[menu][btn];
+  setLed(segment,digit,buttons[menu][btn]);
 }
 
 void setLed(uint8_t row, uint8_t col, bool onoff) {
