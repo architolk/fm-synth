6a3a2da9e8426a42f12e88e1722c8862290cc45c
architolk <msj@xs4all.nl>
2021-03-03 21:16:51 +0100

    Created ratio curve - difference in ratio calculation for DX7 and XFM2

--- dx2xfm/src/main/java/nl/architolk/dx2xfm/Dx2xfm.java
+++ dx2xfm/src/main/java/nl/architolk/dx2xfm/Dx2xfm.java
@@ -41,6 +41,7 @@ public class Dx2xfm {
    private static JSONArray levelcurve;
    private static JSONArray ratecurve;
    private static JSONArray attackcurve;
+   private static JSONArray ratiocurve;
    private static JSONArray paramnames;
    private static JSONArray inits;
 
@@ -49,7 +50,7 @@ public class Dx2xfm {
    	r = (float) rate;
    	x1 = curve.getJSONObject(0).getInt("DX7");
    	y1 = curve.getJSONObject(0).getInt("XFM2");;
-   	for (int i=1;i <5; i++) {  // step through each line segment of the curve
+   	for (int i=1;i <curve.length(); i++) {  // step through each line segment of the curve
       x2 = curve.getJSONObject(i).getInt("DX7");
       y2 = curve.getJSONObject(i).getInt("XFM2");;
    		if (x2 >= r) {  // find the segment this rate (X point) lies on
@@ -119,6 +120,7 @@ public class Dx2xfm {
      levelcurve = loadConfig("curve_level");
      ratecurve = loadConfig("curve_rate");
      attackcurve = loadConfig("curve_attack");
+     ratiocurve = loadConfig("curve_ratio");
      paramnames = loadConfig("DX7Params");
 
      try (
@@ -313,7 +315,7 @@ public class Dx2xfm {
                      mode |= DX7Par[17+(5-op)*21] << (op);
                      XFM2parL[15+op]= DX7Par[18+(5-op)*21];       // RATIO
                      //Ratio 256/100 would actually be "logical", because it is a fine rate between 1.00 and 1.99 at coarse level 1 for DX7 and 1+(0/256) and 1+(255/256) for XFM7: 1.5 = DX7 50 = XFM2 128!
-                     XFM2parL[21+op]= DX7Par[19+(5-op)*21]*256/100; // Back to normal, tried with 150/50 according to rheslip. RATIO FINE (255/99 doesn't seem to be OK - according to code of rheslip)
+                     XFM2parL[21+op]= lscale(DX7Par[19+(5-op)*21],ratiocurve); // Back to normal, tried with 150/50 according to rheslip. RATIO FINE (255/99 doesn't seem to be OK - according to code of rheslip)
 // Check!!
                      XFM2parL[27+op]= 128+(DX7Par[20+(5-op)*21]-7);  // Pitch FINE  neutral   DX7 (0-14)  7 = XFM2 128
 // CHeck!!
